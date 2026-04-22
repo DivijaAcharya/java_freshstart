383113024598f30f047155299787a8e2eca613c9
Divja <acharyadivija.22@gmail.com>
2026-04-22 13:00:58 +0530

    simple ternary operations

---
+++ java_freshstart/src/Main.java
@@ -0,0 +1,28 @@
+   class Main{
+       static void main(String[] args) {
+           // ternary operation on all data types
+           // //condition ? value_if_true : value_if_false;
+
+           //ternary operator for int
+           int a=10,b=20;
+           int c= (a>b) ? a: b;
+           System.out.println(c);
+
+           // ternary operation for double
+           double x=1.2, y = 2.3;
+           double z= (x>y) ? x: y;
+           System.out.println(z);
+
+           // ternary operation for boolean
+           boolean isEven = (a%2==0)? true:false;
+           System.out.println(isEven);
+
+           // ternary operation for String
+           String str1="Apple", str2="orange";
+           String bigString = (str1.length()>str2.length())?str1:str2;
+           System.out.println(bigString);
+       }
+   }
+            // ternary operation on all data types
+    // foreach loop on all data types
+    //two dimensional array loop on all data types

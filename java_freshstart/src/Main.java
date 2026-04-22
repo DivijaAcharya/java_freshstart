b9695f7b88c602558ec44048ce599426d600d94d
Divja <acharyadivija.22@gmail.com>
2026-04-22 15:38:51 +0530

    foreachloop

--- java_freshstart/src/Main.java
+++ java_freshstart/src/Main.java
@@ -1,30 +1,21 @@
-   class TernaryOperation{
+class ForeachLoopExample {
        static void main(String[] args) {
-           // ternary operation on all data types
-           // //condition ? value_if_true : value_if_false;
+     // simple for each loop for an 1D array demo
+        int[] numbers = {1,2,3,4,5};
+        for(int num :numbers){
+            System.out.println("numbers are : "+ num);
+        }
 
-           //ternary operator for int
-           int a=10,b=20;
-           int c= (a>b) ? a: b;
-           System.out.println(c);
+        // demo for 2D array
+           int[][] arr2D = {
+                  {1,2,3,4},
+                   {5,6,7,8,9,10}};
 
-           // ternary operation for double
-           double x=1.2, y = 2.3;
-           double z= (x>y) ? x: y;
-           System.out.println(z);
-
-           // ternary operation for boolean
-           boolean isEven = (a%2==0)? true:false;
-           System.out.println(isEven);
-
-           // ternary operation for String
-           String str1="Apple", str2="orange";
-           String bigString = (str1.length()>str2.length())?str1:str2;
-           System.out.println(bigString);
-
-
+           for (int[] row : arr2D) {
+               for (int val : row) {
+                   System.out.print(val + " ");
+               }
+               System.out.println();
+           }
        }
    }
-            // ternary operation on all data types
-    // foreach loop on all data types
-    //two dimensional array loop on all data types
